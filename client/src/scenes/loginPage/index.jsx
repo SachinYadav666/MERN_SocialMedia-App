60f28718acc81f2c7433018470804a246e72756c
Sachin Yadav <sachintmg666@gmail.com>
2024-01-31 12:00:33 +0530

    change bio

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to SocialMania, the Social Media for Sociopaths!
         </Typography>
         <Form />
       </Box>
